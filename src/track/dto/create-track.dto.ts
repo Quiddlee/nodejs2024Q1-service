a31844a70c10f6f1a7fa18b1eaf961c4e5b3301b
Bogdan <bogdanscherbinadev@gmail.com>
2024-03-07 22:36:44 +0200

    feat: implement basic create track dto validation

--- src/track/dto/create-track.dto.ts
+++ src/track/dto/create-track.dto.ts
@@ -1,9 +1,13 @@
+import { IsNumber, IsString } from 'class-validator';
+
 export class CreateTrackDto {
+  @IsString()
   name: string;
 
   artistId: string | null;
 
   albumId: string | null;
 
+  @IsNumber()
   duration: number;
 }
