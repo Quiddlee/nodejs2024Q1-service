881d5e8443fa8d4afc929231de5ede0aa2c1fac4
Bogdan <bogdanscherbinadev@gmail.com>
2024-03-08 12:29:24 +0200

    fix: create track dto to throw error on empty field

--- src/track/dto/create-track.dto.ts
+++ src/track/dto/create-track.dto.ts
@@ -1,12 +1,14 @@
-import { IsNumber, IsString } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateTrackDto {
   @IsString()
   name: string;
 
   // TODO: create decorator to handle uuid | null ??
+  @IsNotEmpty()
   artistId: string | null;
 
+  @IsNotEmpty()
   albumId: string | null;
 
   @IsNumber()
