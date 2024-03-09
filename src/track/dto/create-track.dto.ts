ab8f3037e9c3187b8a29c9e0b88521a65f18a0ce
Bogdan <bogdanscherbinadev@gmail.com>
2024-03-09 15:47:41 +0200

    fix: change albumId and artistId to be possible empty

--- src/track/dto/create-track.dto.ts
+++ src/track/dto/create-track.dto.ts
@@ -1,14 +1,12 @@
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { IsNumber, IsString } from 'class-validator';
 
 export class CreateTrackDto {
   @IsString()
   name: string;
 
   // TODO: create decorator to handle uuid | null ??
-  @IsNotEmpty()
   artistId: string | null;
 
-  @IsNotEmpty()
   albumId: string | null;
 
   @IsNumber()
