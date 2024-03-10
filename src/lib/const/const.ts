6f58c91fa04598a401d539d5fe24370603848026
Bogdan <bogdanscherbinadev@gmail.com>
2024-03-10 16:17:06 +0200

    feat: implement to automatically generate swagger docs

--- src/lib/const/const.ts
+++ src/lib/const/const.ts
@@ -1,3 +1,5 @@
+import { DocumentBuilder } from '@nestjs/swagger';
+
 export const errorMessage = {
   USER_NOT_FOUND: 'User not found!',
   TRACK_NOT_FOUND: 'Track not found!',
@@ -5,3 +7,10 @@ export const errorMessage = {
   ALBUM_NOT_FOUND: 'Album not found!',
   INVALID_PASSWORD: 'Wrong password provided!',
 } as const;
+
+export const SWAGGER_CONFIG = new DocumentBuilder()
+  .setTitle('Home library')
+  .setDescription('Home library service api')
+  .setVersion('1.0')
+  .addTag('Home Library')
+  .build();
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
 import 'dotenv/config';
 
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
+import { SWAGGER_CONFIG } from './lib/const/const';
 
 const PORT = Number(process.env.PORT);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const document = SwaggerModule.createDocument(app, SWAGGER_CONFIG);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(PORT);
 }
 
